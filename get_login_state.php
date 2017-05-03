aaf1cb7ad57f9a3c8ddfdf444d2f601ee521ce0c
Marek Dědič <developer@dedic.eu>
2017-05-03 22:11:53 +0200

    Merge branch 'master' into skautis-account-avatar

--- get_login_state.php
+++ get_login_state.php
@@ -55,6 +55,9 @@ function showUserAccount($skautis)
 	$idPerson = $skautis->UserManagement->UserDetail()->ID_Person;
 	$response['user_name'] = $skautis->OrganizationUnit->PersonDetail(array('ID' => $idPerson))->DisplayName;
 	$response['role'] = getRole($idPerson);
+	$response['user_avatar'] = base64_encode($skautis->OrganizationUnit->PersonPhoto(array(
+		'ID' => $idPerson,
+		'Size' => 'small'))->PhotoSmallContent);
 	return $response;
 }
 
