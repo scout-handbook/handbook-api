103a35eafcf07fc5801680acedd05369a98b931c
GenaBitu <genabitu@gmail.com>
2018-04-05 07:59:17 +0200

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 set -e
-URLS=$(echo "    urls: [$(ls -d */ | grep -v "vendor" | awk -v ORS= '{print "{url: \"https://raw.githubusercontent.com/GenaBitu/OdyMaterialy/master/API/" $1 "/swagger.yaml\", name: \"Version " substr($1, 2) "\"}, "}' | sed 's/..$//')],")
+URLS=$(echo "    urls: [$(ls -d */ | grep -v "vendor" | awk -v ORS= '{print "{url: \"https://raw.githubusercontent.com/GenaBitu/HandbookAPI/master/" $1 "swagger.yaml\", name: \"Version " substr($1, 2) "\"}, "}' | sed 's/..$//')],")
 git clone https://github.com/swagger-api/swagger-ui.git
 cd swagger-ui
 npm install
