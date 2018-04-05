697b8b7aa249f14cbb0aaf217a3dd589b87c4db5
Marek Dědič <developer@dedic.eu>
2018-04-05 08:12:58 +0200

    Renaming to HandbookAPI

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ URLS=$(echo "    urls: [$(ls -d */ | grep -v "vendor" | awk -v ORS= '{print "{ur
 git clone https://github.com/swagger-api/swagger-ui.git
 cd swagger-ui
 npm install
-sed -i -e "/<title>Swagger UI<\/title>/c <title>OdyMateriály API</title>" dist/index.html
+sed -i -e "/<title>Swagger UI<\/title>/c <title>Handbook API</title>" dist/index.html
 sed -i -e "/url: \"http:\/\/petstore\.swagger\.io\/v2\/swagger\.json\",/c \\${URLS}" dist/index.html
 grep -Fq "$URLS" dist/index.html
 npm run build
