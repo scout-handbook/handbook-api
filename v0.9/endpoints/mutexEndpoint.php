d4457e26eb87cc6772f4594cd309403545a764c2
Marek Dědič <developer@dedic.eu>
2018-04-12 10:59:22 +0200

    Added untested mutex add operation

--- v0.9/endpoints/mutexEndpoint.php
+++ v0.9/endpoints/mutexEndpoint.php
@@ -10,6 +10,64 @@ $mutexEndpoint = new HandbookAPI\Endpoint();
 
 $addMutex = function(Skautis\Skautis $skautis) : array
 {
+	$selectSQL = <<<SQL
+	SELECT DISTINCT UNIX_TIMESTAMP(mutexes.timeout), mutexes.holder, users.name
+FROM mutexes
+LEFT JOIN users ON mutexes.holder = users.id
+WHERE mutexes.id = :id;
+SQL;
+	$deleteSQL = <<<SQL
+DELETE FROM mutexes
+WHERE id = :id;
+SQL;
+	$insertSQL = <<<SQL
+INSERT INTO mutexes (id, timeout, holder)
+VALUES (:id, FROM_UNIXTIME(:timeout), :holder);
+SQL;
+
+	$id = HandbookAPI\Helper::parseUuid($data['id'], 'resource')->getBytes();
+	$timeout = time() + 1800;
+	if(isset($_COOKIE['skautis_timeout']))
+	{
+		$timeout = ctype_digit($_COOKIE['skautis_timeout']) ? intval($_COOKIE['skautis_timeout']) : null;
+		if($timeout === null || abs($timeout - time()) > 3600)
+		{
+			throw new HandbookAPI\InvalidArgumentTypeException('number', ['Unix timestamp']);
+		}
+	}
+	$userId = $skautis->UserManagement->LoginDetail()->ID_Person;
+
+	$db = new HandbookAPI\Database();
+	$db->beginTransaction();
+
+	$db->prepare($selectSQL);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->execute();
+	$origTimeout = '';
+	$origHolder = '';
+	$origHolderName = '';
+	$db->bindColumn(1, $origTimeout);
+	$db->bindColumn('holder', $origHolder);
+	$db->bindColumn('name', $origHolderName);
+	if($db->fetch())
+	{
+		if($origHolder != $userId && $origTimeout > time())
+		{
+			return ['status' => 409, 'response' => ['holder' => $origHolderName]];
+		}
+	}
+
+	$db->prepare($deleteSQL);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->execute();
+
+	$db->prepare($insertSQL);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->bindParam(':timeout', $timeout, PDO::PARAM_INT);
+	$db->bindParam(':holder', $userId, PDO::PARAM_INT);
+	$db->execute();
+
+	$db->endTransaction();
 	return ['status' => 200];
 };
 $mutexEndpoint->setAddMethod(new HandbookAPI\Role('editor'), $addMutex);
