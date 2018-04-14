3f63b7040bbde9f48096461ddfd563b6b25a56d6
GenaBitu <genabitu@gmail.com>
2018-04-14 15:50:11 +0200

    Mutex extending and releasing, not tested

--- v0.9/endpoints/mutexEndpoint.php
+++ v0.9/endpoints/mutexEndpoint.php
@@ -8,10 +8,10 @@ require_once($CONFIG->basepath . '/v0.9/internal/Role.php');
 
 $mutexEndpoint = new HandbookAPI\Endpoint();
 
-$addMutex = function(Skautis\Skautis $skautis) : array
+$addMutex = function(Skautis\Skautis $skautis, array $data) : array
 {
 	$selectSQL = <<<SQL
-	SELECT DISTINCT UNIX_TIMESTAMP(mutexes.timeout), mutexes.holder, users.name
+SELECT DISTINCT UNIX_TIMESTAMP(mutexes.timeout), mutexes.holder, users.name
 FROM mutexes
 LEFT JOIN users ON mutexes.holder = users.id
 WHERE mutexes.id = :id;
@@ -68,6 +68,86 @@ SQL;
 	$db->execute();
 
 	$db->endTransaction();
-	return ['status' => 200];
+	return ['status' => 201];
 };
 $mutexEndpoint->setAddMethod(new HandbookAPI\Role('editor'), $addMutex);
+
+$extendMutex = function(Skautis\Skautis $skautis, array $data) : array
+{
+	$selectSQL = <<<SQL
+SELECT 1
+FROM mutexes
+WHERE id = :id AND holder = :holder;
+SQL;
+	$updateSQL = <<<SQL
+UPDATE mutexes
+SET timeout = FROM_UNIXTIME(:timeout)
+WHERE id = :id AND holder = :holder
+LIMIT 1;
+SQL;
+
+	$id = HandbookAPI\Helper::parseUuid($data['id'], 'resource')->getBytes();
+	$timeout = time() + 1800;
+	if(isset($_COOKIE['skautis_timeout']))
+	{
+		$timeout = ctype_digit($_COOKIE['skautis_timeout']) ? intval($_COOKIE['skautis_timeout']) : null;
+		if($timeout === null || abs($timeout - time()) > 3600)
+		{
+			throw new HandbookAPI\InvalidArgumentTypeException('number', ['Unix timestamp']);
+		}
+	}
+	$userId = $skautis->UserManagement->LoginDetail()->ID_Person;
+
+	$db = new HandbookAPI\Database();
+	$db->beginTransaction();
+
+	$db->prepare($selectSQL);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->bindParam(':holder', $userId, PDO::PARAM_INT);
+	$db->execute();
+	$db->fetchRequire('mutex');
+
+	$db->prepare($updateSQL);
+	$db->bindParam(':timeout', $timeout, PDO::PARAM_INT);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->bindParam(':holder', $userId, PDO::PARAM_INT);
+	$db->execute();
+
+	$db->endTransaction();
+	return ['status' => 200];
+};
+$mutexEndpoint->setUpdateMethod(new HandbookAPI\Role('editor'), $extendMutex);
+
+$releaseMutex = function(Skautis\Skautis $skautis, array $data) : array
+{
+	$selectSQL = <<<SQL
+SELECT 1
+FROM mutexes
+WHERE id = :id AND holder = :holder;
+SQL;
+	$deleteSQL = <<<SQL
+DELETE FROM mutexes
+WHERE id = :id AND holder = :holder;
+SQL;
+
+	$id = HandbookAPI\Helper::parseUuid($data['id'], 'resource')->getBytes();
+	$userId = $skautis->UserManagement->LoginDetail()->ID_Person;
+
+	$db = new HandbookAPI\Database();
+	$db->beginTransaction();
+
+	$db->prepare($selectSQL);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->bindParam(':holder', $userId, PDO::PARAM_INT);
+	$db->execute();
+	$db->fetchRequire('mutex');
+
+	$db->prepare($deleteSQL);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->bindParam(':holder', $userId, PDO::PARAM_INT);
+	$db->execute();
+	
+	$db->endTransaction();
+	return ['status' => 200];
+};
+$mutexEndpoint->setDeleteMethod(new HandbookAPI\Role('editor'), $releaseMutex);
