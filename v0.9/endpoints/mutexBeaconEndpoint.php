418e01a8da0d36a70f78f4d2d2bcafb1e22e038a
Marek Dědič <developer@dedic.eu>
2018-04-24 19:10:22 +0200

    Initial implementation of mutex-beacon endpoint

---
+++ v0.9/endpoints/mutexBeaconEndpoint.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types = 1);
+@_API_EXEC === 1 or die('Restricted access.');
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/api-config.php');
+require_once($CONFIG->basepath . '/vendor/autoload.php');
+require_once($CONFIG->basepath . '/v0.9/internal/Database.php');
+require_once($CONFIG->basepath . '/v0.9/internal/Endpoint.php');
+require_once($CONFIG->basepath . '/v0.9/internal/Helper.php');
+require_once($CONFIG->basepath . '/v0.9/internal/Role.php');
+
+$mutexBeaconEndpoint = new HandbookAPI\Endpoint();
+
+$releaseBeaconMutex = function(Skautis\Skautis $skautis, array $data) : array
+{
+	$selectSQL = <<<SQL
+SELECT 1
+FROM mutexes
+WHERE id = :id AND holder = :holder;
+SQL;
+	$deleteSQL = <<<SQL
+DELETE FROM mutexes
+WHERE id = :id AND holder = :holder;
+SQL;
+
+	try
+	{
+		$id = HandbookAPI\Helper::parseUuid($data['id'], 'resource')->getBytes();
+		$userId = $skautis->UserManagement->LoginDetail()->ID_Person;
+
+		$db = new HandbookAPI\Database();
+		$db->beginTransaction();
+
+		$db->prepare($selectSQL);
+		$db->bindParam(':id', $id, PDO::PARAM_STR);
+		$db->bindParam(':holder', $userId, PDO::PARAM_INT);
+		$db->execute();
+		$db->fetchRequire('mutex');
+
+		$db->prepare($deleteSQL);
+		$db->bindParam(':id', $id, PDO::PARAM_STR);
+		$db->bindParam(':holder', $userId, PDO::PARAM_INT);
+		$db->execute();
+		
+		$db->endTransaction();
+	}
+	catch(Exception $e)
+	{
+		die();
+	}
+	die();
+};
+$mutexEndpoint->setAddMethod(new HandbookAPI\Role('editor'), $releaseBeaconMutex);
---
+++ v0.9/mutex-beacon.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types = 1);
+const _API_EXEC = 1;
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/api-config.php');
+require_once($CONFIG->basepath . '/v0.9/endpoints/mutexBeaconEndpoint.php');
+
+$mutexBeaconEndpoint->handle();
