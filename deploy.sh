afd050578a2bcf0d487316ae7127f11a273fd4dd
GenaBitu <genabitu@gmail.com>
2018-10-30 23:03:45 +0100

    Travis swagger with npm

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 set -e
-URLS=$(echo "    urls: [$(ls -d */ | grep -v "vendor" | awk -v ORS= '{print "{url: \"https://raw.githubusercontent.com/GenaBitu/HandbookAPI/master/" $1 "swagger.yaml\", name: \"Version " substr($1, 2) "\"}, "}' | sed 's/..$//')],")
-git clone https://github.com/swagger-api/swagger-ui.git
-cd swagger-ui
-npm install
-sed -i -e "/<title>Swagger UI<\/title>/c <title>Handbook API</title>" dist/index.html
-sed -i -e "/url: \"http:\/\/petstore\.swagger\.io\/v2\/swagger\.json\",/c \\${URLS}" dist/index.html
-grep -Fq "$URLS" dist/index.html
-npm run build
+URLS=$(echo "    urls: [$(ls -d */ | grep -Ev "node_modules|vendor" | awk -v ORS= '{print "{url: \"https://raw.githubusercontent.com/GenaBitu/HandbookAPI/master/" $1 "swagger.yaml\", name: \"Version " substr($1, 2) "\"}, "}' | sed 's/..$//')],")
+sed -i -e "/<title>Swagger UI<\/title>/c <title>Handbook API</title>" node_modules/swagger-ui-dist/index.html
+sed -i -e "/url: \"https:\/\/petstore\.swagger\.io\/v2\/swagger\.json\",/c \\${URLS}" node_modules/swagger-ui-dist/index.html
