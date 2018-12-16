55a4e4a11f02abda0013e49081d0e060f643aa4c
Marek Dědič <developer@dedic.eu>
2018-12-16 18:45:39 +0100

    Redirected everything to GitLab

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 set -e
-URLS=$(echo "    urls: [$(ls -d */ | grep -E "v.*\\..*" | awk -v ORS= '{print "{url: \"https://raw.githubusercontent.com/GenaBitu/HandbookAPI/master/" $1 "swagger.yaml\", name: \"Version " substr($1, 2) "\"}, "}' | sed 's/..$//')],")
+URLS=$(echo "    urls: [$(ls -d */ | grep -E "v.*\\..*" | awk -v ORS= '{print "{url: \"https://gitlab.com/skaut-handbook/handbook-api/raw/master/" $1 "swagger.yaml\", name: \"Version " substr($1, 2) "\"}, "}' | sed 's/..$//')],")
 sed -i -e "/<title>Swagger UI<\/title>/c <title>Handbook API</title>" node_modules/swagger-ui-dist/index.html
 sed -i -e "/url: \"https:\/\/petstore\.swagger\.io\/v2\/swagger\.json\",/c \\${URLS}" node_modules/swagger-ui-dist/index.html
