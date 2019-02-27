68fd5327d71ee5c48d5f96aa33d878d2d780e1af
GenaBitu <genabitu@gmail.com>
2019-02-27 14:05:42 +0100

    Autoloading FullField

--- v0.9/endpoints/fieldEndpoint.php
+++ v0.9/endpoints/fieldEndpoint.php
@@ -5,13 +5,14 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/api-config.php');
 require_once($CONFIG->basepath . '/vendor/autoload.php');
 require_once($CONFIG->basepath . '/v0.9/internal/Database.php');
 require_once($CONFIG->basepath . '/v0.9/internal/Endpoint.php');
-require_once($CONFIG->basepath . '/v0.9/internal/FullField.php');
 require_once($CONFIG->basepath . '/v0.9/internal/Helper.php');
 require_once($CONFIG->basepath . '/v0.9/internal/Role.php');
 
 require_once($CONFIG->basepath . '/v0.9/internal/exceptions/MissingArgumentException.php');
 
 use Ramsey\Uuid\Uuid;
+
+use Skaut\HandbookAPI\v0_9\FullField;
 
 $fieldEndpoint = new HandbookAPI\Endpoint();
 
@@ -34,7 +35,7 @@ SQL;
     $db->bindColumn('image', $field_image);
     $fields = [];
     while ($db->fetch()) {
-        $fields[] = new HandbookAPI\FullField($field_id, $field_name, $field_description, $field_image);
+        $fields[] = new FullField($field_id, $field_name, $field_description, $field_image);
     }
     return ['status' => 200, 'response' => $fields];
 };
