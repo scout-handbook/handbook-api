adb54863b1d4fb54c6fa02ef44afd9ab7496d694
Marek Dědič <developer@dedic.eu>
2019-02-27 14:05:42 +0100

    Autoloading FullField

--- internal/v0_9/FullField.php
+++ internal/v0_9/FullField.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
-namespace HandbookAPI;
+namespace Skaut\HandbookAPI\v0_9;
 
 @_API_EXEC === 1 or die('Restricted access.');
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/api-config.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/api-config.php');
 require_once($CONFIG->basepath . '/vendor/autoload.php');
 require_once($CONFIG->basepath . '/v0.9/internal/Helper.php');
 
@@ -17,8 +17,8 @@ class FullField implements \JsonSerializable
     public function __construct(string $id, string $name, $description, string $image)
     {
         $this->id = \Ramsey\Uuid\Uuid::fromBytes($id);
-        $this->name = Helper::xssSanitize($name);
-        $this->description = Helper::xssSanitize($description);
+        $this->name = \HandbookAPI\Helper::xssSanitize($name);
+        $this->description = \HandbookAPI\Helper::xssSanitize($description);
         $this->image = \Ramsey\Uuid\Uuid::fromBytes($image);
     }
 
