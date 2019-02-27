9a41cabb536648315872b7abaa09381e5df93d1f
Marek Dědič <developer@dedic.eu>
2019-02-27 18:02:36 +0100

    Autoloading Group

--- HandbookAPI/v0_9/Group.php
+++ HandbookAPI/v0_9/Group.php
@@ -1,14 +1,12 @@
 <?php declare(strict_types=1);
-namespace HandbookAPI;
+namespace Skaut\HandbookAPI\v0_9;
 
 @_API_EXEC === 1 or die('Restricted access.');
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/api-config.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/api-config.php');
 require_once($CONFIG->basepath . '/vendor/autoload.php');
 
 use Ramsey\Uuid\Uuid;
-
-use Skaut\HandbookAPI\v0_9\Helper;
 
 class Group implements \JsonSerializable
 {
