eb26454bb6e6c19126e9006599326748cdc562ca
GenaBitu <genabitu@gmail.com>
2019-03-03 23:16:12 +0100

    Moved LessonContainer_cmp into LessonContainer

--- Skaut/HandbookAPI/v0_9/LessonContainer.php
+++ Skaut/HandbookAPI/v0_9/LessonContainer.php
@@ -10,4 +10,28 @@ class LessonContainer
     public function __construct()
     {
     }
+
+    // Container comparison function used in usort. Assumes that both Containers have their lessons sorted low-to-high.
+    public static function compare(LessonContainer $first, LessonContainer $second) : int
+    {
+        if (get_class($first) === 'Skaut\HandbookAPI\v0_9\LessonContainer') {
+            if (get_class($second) === 'Skaut\HandbookAPI\v0_9\LessonContainer') {
+                return 0;
+            }
+            return -1;
+        }
+        if (get_class($second) === 'Skaut\HandbookAPI\v0_9\LessonContainer') {
+            return 1;
+        }
+        if (empty($first->lessons)) {
+            if (empty($second->lessons)) {
+                return 0;
+            }
+            return -1;
+        }
+        if (empty($second->lessons)) {
+            return 1;
+        }
+        return Lesson::compare($first->lessons[0], $second->lessons[0]);
+    }
 }
