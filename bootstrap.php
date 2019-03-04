7c584e002192c6f588693664907fdcde9e71cda0
GenaBitu <genabitu@gmail.com>
2019-03-04 23:45:16 +0100

    Added phpstan bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+const _API_EXEC = 1;
+const _API_SECRETS_EXEC = 1;
+
+$CONFIG = (object)[
+        'basepath' => $_SERVER['DOCUMENT_ROOT'] . '/API',
+        'imagepath' => $_SERVER['DOCUMENT_ROOT'] . '/images',
+        'cookieuri' => 'odymaterialy.skauting.cz',
+        'baseuri' => 'https://odymaterialy.skauting.cz',
+        'apiuri' => 'https://odymaterialy.skauting.cz/API/v0.9'
+    ];
