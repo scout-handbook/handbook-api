2f9460d6a8618c3ff14fa9974e9e0a3078baa902
GenaBitu <genabitu@gmail.com>
2019-03-23 23:56:59 +0100

    Updated lesson list endpoint

--- Skaut/HandbookAPI/v1_0/Lesson.php
+++ Skaut/HandbookAPI/v1_0/Lesson.php
@@ -9,24 +9,15 @@ use Skaut\HandbookAPI\v1_0\Helper;
 
 class Lesson implements \JsonSerializable
 {
-    private $id;
     private $name;
     private $version;
     private $competences;
-    private $lowestCompetence;
 
-    public function __construct(string $id, string $name, float $version)
+    public function __construct(string $name, float $version)
     {
-        $this->id = Uuid::fromBytes($id);
         $this->name = Helper::xssSanitize($name);
         $this->version = round($version * 1000);
         $this->competences = [];
-        $this->lowestCompetence = 0;
-    }
-
-    public function setLowestCompetence(int $competence) : void
-    {
-        $this->lowestCompetence = $competence;
     }
 
     public function addCompetence(string $competence) : void
@@ -37,29 +28,9 @@ class Lesson implements \JsonSerializable
     public function jsonSerialize() : array
     {
         return [
-            'id' => $this->id,
             'name' => $this->name,
             'version' => $this->version,
             'competences' => $this->competences
         ];
-    }
-
-    // Lesson comparison function used in usort.
-    // Assumes that both Lessons have their competences field sorted low-to-high.
-    public static function compare(Lesson $first, Lesson $second) : int
-    {
-        if (empty($first->competences)) {
-            if (empty($second->competences)) {
-                return 0;
-            }
-            return -1;
-        }
-        if (empty($second->competences)) {
-            return 1;
-        }
-        if ($first->lowestCompetence === $second->lowestCompetence) {
-            return 0;
-        }
-        return ($first->lowestCompetence < $second->lowestCompetence) ? -1 : 1;
     }
 }
