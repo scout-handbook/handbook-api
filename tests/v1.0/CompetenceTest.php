7c57d5ff0e678c58808ba9781c7512c3eb6e8ae0
Marek Dědič <developer@dedic.eu>
2019-03-24 01:58:21 +0100

    Updated unit tests

--- tests/v1.0/CompetenceTest.php
+++ tests/v1.0/CompetenceTest.php
@@ -14,12 +14,7 @@ class CompetenceTest extends TestCase
      */
     public function testCtor() : Competence
     {
-        $competence = new Competence(
-            pack('H*', '1739a63aa2544a959508103b7c80bcdb'),
-            42,
-            'cname',
-            'cdescription'
-        );
+        $competence = new Competence(42, 'cname', 'cdescription');
         $this->assertInstanceOf('\Skaut\HandbookAPI\v1_0\Competence', $competence);
         return $competence;
     }
@@ -31,17 +26,8 @@ class CompetenceTest extends TestCase
     public function testJsonSerialize(Competence $competence) : void
     {
         $this->assertJsonStringEqualsJsonString(
-            '{"id":"1739a63a-a254-4a95-9508-103b7c80bcdb","number":42,"name":"cname","description":"cdescription"}',
+            '{"number":42,"name":"cname","description":"cdescription"}',
             json_encode($competence)
         );
-    }
-
-    /**
-     * @covers Skaut\HandbookAPI\v1_0\Competence::__construct()
-     */
-    public function testCtorInvalid() : void
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        new Competence(pack('H*', '2a0205609ddf4694b8ac8f846a195865f'), 42, 'cname', 'cdescription');
     }
 }
--- tests/v1.0/DeletedLessonTest.php
+++ tests/v1.0/DeletedLessonTest.php
@@ -14,7 +14,7 @@ class DeletedLessonTest extends TestCase
      */
     public function testCtor()
     {
-        $deletedLesson = new DeletedLesson(pack('H*', '1739a63aa2544a959508103b7c80bcdb'), 'dlname');
+        $deletedLesson = new DeletedLesson('dlname');
         $this->assertInstanceOf('\Skaut\HandbookAPI\v1_0\DeletedLesson', $deletedLesson);
         return $deletedLesson;
     }
@@ -26,17 +26,8 @@ class DeletedLessonTest extends TestCase
     public function testJsonSerialize(DeletedLesson $deletedLesson) : void
     {
         $this->assertJsonStringEqualsJsonString(
-            '{"id":"1739a63a-a254-4a95-9508-103b7c80bcdb","name":"dlname"}',
+            '{"name":"dlname"}',
             json_encode($deletedLesson)
         );
-    }
-
-    /**
-     * @covers Skaut\HandbookAPI\v1_0\DeletedLesson::__construct()
-     */
-    public function testCtorInvalid()
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        new DeletedLesson(pack('H*', '1739a63aa2544a959508103b7c80bcdbf'), 'dlname');
     }
 }
