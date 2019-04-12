8279cecc7ae6a116473ed3c280ca571b9f71f1dd
GenaBitu <genabitu@gmail.com>
2019-04-12 15:14:36 +0200

    Fixed PDF header being on the wrong side

--- v0.9/endpoints/lessonPDFEndpoint.php
+++ v0.9/endpoints/lessonPDFEndpoint.php
@@ -111,7 +111,7 @@ SQL;
     $mpdf->WriteHTML('', 2);
     $mpdf->SetHTMLHeaderByName('OddHeader', 'O');
 
-    $mpdf->WriteHTML(file_get_contents($CONFIG->basepath . '/Skaut/OdyMarkdown/v0_9/styles.php') ?: '', 1);
+    $mpdf->WriteHTML(file_get_contents($CONFIG->apiuri . '/Skaut/OdyMarkdown/v0_9/styles.php') ?: '', 1);
     $mpdf->WriteHTML($html, 2);
 
     header('content-type:application/pdf; charset=utf-8');
