05f3546c3f6e40d00798a747542899ff7858a7e6
Marek Dědič <developer@dedic.eu>
2024-02-19 22:26:08 +0100

    Using standard lines for line page

--- Skaut/OdyMarkdown/v1_0/OdyMarkdown.php
+++ Skaut/OdyMarkdown/v1_0/OdyMarkdown.php
@@ -141,7 +141,7 @@ class OdyMarkdown extends GithubMarkdown
             if ($block['pocet'] === 'strana') {
                 $pgbr = $block['lastPage'] ? '' : '<pagebreak>';
                 if ($dotted) {
-                    return '<br><div class="dottedpage">' . str_repeat('<hr class="dottedlineh">', 32) . '</div>' . $pgbr;
+                    return '<br><div class="dottedpage">' . str_repeat('<br><div class="dottedline">' . str_repeat('.', 256) . '</div>', 32) . '</div>' . $pgbr;
                 }
                 return $pgbr;
             } else {
