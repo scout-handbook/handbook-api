8f8b694aa644df23b04541d3b1dcc09b50640bf2
Marek Dědič <developer@dedic.eu>
2025-07-05 12:18:55 +0200

    Added feature test setup

---
+++ tests/api-config.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+@_API_EXEC === 1 or die('Restricted access.');
+
+$CONFIG = (object)[
+        'basepath' => $_SERVER['DOCUMENT_ROOT'] . '/../legacy',
+        'imagepath' => $_SERVER['DOCUMENT_ROOT'] . '/images',
+        'cookieuri' => 'odymaterialy.test',
+        'baseuri' => 'https://odymaterialy.test',
+        'apiuri' => 'https://odymaterialy.test/API'
+    ];
