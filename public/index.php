9ccde04aa4a4634f7944de0cdf9c57d87a5141e1
Marek Dědič <developer@dedic.eu>
2025-07-10 01:42:52 +0200

    Removed assignment if if

--- public/index.php
+++ public/index.php
@@ -18,7 +18,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+$maintenance = __DIR__.'/../storage/framework/maintenance.php';
+if (file_exists($maintenance)) {
     require $maintenance;
 }
 
