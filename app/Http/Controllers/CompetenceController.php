8fdea73cf3a83ceded9e340b084bd0503e88ed1c
Marek Dědič <developer@dedic.eu>
2025-07-12 22:52:07 +0200

    Added competence adding endpoint

--- app/Http/Controllers/CompetenceController.php
+++ app/Http/Controllers/CompetenceController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers;
 
 use App\Competence;
 use App\Http\Resources\CompetenceCollection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 final class CompetenceController extends Controller
 {
@@ -14,12 +16,20 @@ final class CompetenceController extends Controller
         return new CompetenceCollection(Competence::all());
     }
 
-    /*
-    public static function store(Request $request)
+    public static function store(Request $request): JsonResponse
     {
-        // TODO
+        $competence = new Competence;
+
+        $competence->description = $request->description ?? '';
+        $competence->name = $request->name;
+        $competence->number = $request->number;
+
+        $competence->save();
+
+        return new JsonResponse(['status' => 201], 201);
     }
 
+    /*
     public static function update(Request $request, Competence $competence)
     {
         // TODO
