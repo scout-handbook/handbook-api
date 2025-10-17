67ecb7d5f9b74c24af365402f18894d4510155de
marekdedic <developer@dedic.eu>
2025-10-17 20:11:44 +0000

    Deploying to gh-pages from @ scout-handbook/handbook-api@6b0b806af4991fd545f4c83e34d07a90bd7be39e 🚀

---
+++ swagger-initializer.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    urls: [{url: "https://scout-handbook.github.io/handbook-api/definitions/v1.0.yaml", name: "Version 1.0"}],
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
